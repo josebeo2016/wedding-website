d20c511beb15c618a1e244049901be74f20d40f6
Joseph Phúc <josebeo2016@gmail.com>
2023-09-17 00:25:51 +0900

    update slide show with swiper

---
+++ slidegen.py
@@ -0,0 +1,12 @@
+import os
+
+images = [image for image in os.listdir('img/album')]
+
+for image in images:
+    code = f'''
+    <div class="swiper-slide">
+        <img src="img/album/{image}" loading="lazy"/>
+        <div class="swiper-lazy-preloader"></div>
+    </div>
+    '''
+    print(code)
