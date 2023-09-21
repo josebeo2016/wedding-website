d887c2af11edd9b0fcb859b0f0e6a867e9c74797
josebeo2016 <josebeo2016@gmail.com>
2023-09-21 22:33:46 +0900

    countdown

--- js/scripts.js
+++ js/scripts.js
@@ -183,6 +183,53 @@ $(document).ready(function () {
         $('#btn-show-content').toggleClass('toggle-map-content');
     });
 
+    /********************** Countdown **********************/
+    if ($("#clock").length) {
+        function timeElapse(date){
+            var current = Date();
+            var seconds = (Date.parse(current) - Date.parse(date)) / 1000;
+            var days = Math.floor(seconds / (3600 * 24));
+            if (days < 10) {
+                days = "0" + days;
+            }
+            seconds = seconds % (3600 * 24);
+            var hours = Math.floor(seconds / 3600);
+            if (hours < 10) {
+                hours = "0" + hours;
+            }
+            seconds = seconds % 3600;
+            var minutes = Math.floor(seconds / 60);
+            if (minutes < 10) {
+                minutes = "0" + minutes;
+            }
+            seconds = seconds % 60;
+            if (seconds < 10) {
+                seconds = "0" + seconds;
+            }
+			var html = '<div class="timer-item"><div><span class="days digit">' + days + '</span> <div class="smalltext">'+ $('#clock').data('text-day') +'</div></div></div><div class="timer-item"><div><span class="hours digit">' + hours + '</span> <div class="smalltext">'+ $('#clock').data('text-hour') +'</div></div></div><div class="timer-item"><div><span class="minutes digit">' + minutes + '</span> <div class="smalltext">'+ $('#clock').data('text-minute') +'</div></div></div><div class="timer-item"><div><span class="seconds digit">' + seconds + '</span> <div class="smalltext">'+ $('#clock').data('text-second') +'</div></div></div>';
+            $('#clock').html(html);
+        }
+
+        $('#clock').countdown($('#clock').data('date'), function(event) {
+            if(event.type == 'stoped'){
+                var together = new Date($('#clock').data('date'));           
+                together.setHours(0);                           
+                together.setMinutes(0);             
+                together.setSeconds(0);                 
+                together.setMilliseconds(0);
+                setInterval(function() {
+                    timeElapse(together);
+                }, 1000);
+            }else{
+               var $this = $(this).html(event.strftime(''
+                + '<div class="timer-item"><div><span class="days digit">%D</span> <div class="smalltext">'+ $('#clock').data('text-day') +'</div></div></div>'
+                + '<div class="timer-item"><div><span class="hours digit">%H</span> <div class="smalltext">'+ $('#clock').data('text-hour') +'</div></div></div>'
+                + '<div class="timer-item"><div><span class="minutes digit">%M</span> <div class="smalltext">'+ $('#clock').data('text-minute') +'</div></div></div>'
+                + '<div class="timer-item"><div><span class="seconds digit">%S</span> <div class="smalltext">'+ $('#clock').data('text-second') +'</div></div></div>'));
+            }
+        });
+    }
+
     /********************** Add to Calendar **********************/
     var myCalendar = createCalendar({
         options: {
